bfe6025be3c12fa5c7297d4e1f643d759a4080ef
youga114 <youga114@gmail.com>
2021-09-18 15:00:53 +0900

    [2869] Add answer

--- problem/2869/main/main.cc
+++ problem/2869/main/main.cc
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main(int argc, char** argv) {
-    long long A, B, V;
+    double A, B, V;
     cin >> A >> B >> V;
 
-    cout << ((V - A) / (A - B)) + 1;
+    cout << static_cast<int>(ceil((V - A) / (A - B))) + 1;
 
     return 0;
 }
