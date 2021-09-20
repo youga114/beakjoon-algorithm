8ec4172ed65eb191b24b0cb5a20466da1b0d6ac3
youga114 <youga114@gmail.com>
2021-09-20 16:44:34 +0900

    [2751] Add answer

---
+++ problem/2751/main/main.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    int n;
+    vector<int> v;
+
+    cin >> n;
+
+    for(int i = 0; i < n; ++i) {
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
+    }
+
+    sort(v.begin(), v.end());
+    for(int i = 0; i < n; ++i) {
+        cout << v[i] << '\n';
+    }
+
+    return 0;
+}
