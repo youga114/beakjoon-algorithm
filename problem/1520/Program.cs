d77676bfd94f30107090f071682e64329fb9cdc9
yubie <youga114@gmail.com>
2022-04-19 18:44:53 +0900

    [1520] Add process

---
+++ problem/1520/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1520
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n, maxValue = 10001;
+            
+            List<string> mn = Console.ReadLine().Split(" ").ToList();
+            m = Convert.ToInt32(mn[0]);
+            n = Convert.ToInt32(mn[1]);
+
+            var map = new List<List<int>>();
+            map.Add(new List<int>(new int[n + 2]));
+            for(int i = 0; i < m; ++i)
+            {
+                var line = new List<int>();
+                line.Add(maxValue);
+                line.AddRange(Console.ReadLine().Split(" ").ToList().Select(x => Convert.ToInt32(x)));
+                line.Add(maxValue);
+                map.Add(line);
+            }
+            map.Add(new List<int>(new int[n + 2]));
+            
+            var visitedCnt = new List<List<int>>();
+            for (int i = 0; i < m + 2; ++i)
+            {
+                visitedCnt.Add(new List<int>(new int[n + 2]));
+            }
+            for (int i = 1; i < m + 1; ++i)
+            {
+                for (int j = 1; j < n + 1; ++j)
+                {
+                    visitedCnt[i][j] = -1;
+                }
+            }
+            visitedCnt[1][1] = 1;
+
+            Console.Write(getCnt(m, n, map, visitedCnt, map[m][n] - 1));
+        }
+        
+        static int getCnt(int x, int y, List<List<int>> map, List<List<int>> visitedCnt, int prevH)
+        {
+            if (visitedCnt[x][y] != -1)
+            {
+                return visitedCnt[x][y];
+            }
+
+            if (prevH <= map[x][y])
+            {
+                return 0;
+            }
+            
+            visitedCnt[x][y] = 0;
+            visitedCnt[x][y] = getCnt(x + 1, y, map, visitedCnt, map[x][y]) + getCnt(x - 1, y, map, visitedCnt, map[x][y]) + getCnt(x, y + 1, map, visitedCnt, map[x][y]) + getCnt(x, y - 1, map, visitedCnt, map[x][y]);
+
+            return visitedCnt[x][y];
+        }
+    }
+}
