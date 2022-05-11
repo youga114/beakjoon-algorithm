ffee05c2edb2fb5712b7a9f6e1f0eb22af388295
yubie <youga114@gmail.com>
2022-05-11 18:54:20 +0900

    [10816] Add answer

---
+++ problem/10816/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _10816
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            var allNum = Console.ReadLine().Split(" ");
+
+            var numCnt = new int[20000001];
+
+            foreach(var num in allNum)
+            {
+                numCnt[Convert.ToInt32(num) + 10000000]++;
+            }
+
+            int receivedCnt = Convert.ToInt32(Console.ReadLine());
+
+            var receivedNum = Console.ReadLine().Split(" ");
+
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach(var num in receivedNum)
+            {
+                stringBuilder.Append(numCnt[Convert.ToInt32(num) + 10000000] + " ");
+            }
+            Console.WriteLine(stringBuilder.ToString());
+        }
+    }
+}
