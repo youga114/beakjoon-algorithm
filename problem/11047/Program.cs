e720cf40979522cd42e25851fc40afa8e5ef4957
yubie <youga114@gmail.com>
2022-05-18 17:43:40 +0900

    [11047] Add answer

---
+++ problem/11047/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11047
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k, answer = 0;
+
+            var nk = Console.ReadLine().Split(" ").ToList();
+
+            n = Convert.ToInt32(nk[0]);
+            k = Convert.ToInt32(nk[1]);
+
+            var coins = new List<int>();
+            for (int i = 0; i < n; ++i)
+            {
+                coins.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            for (int i = coins.Count - 1; i >= 0; --i)
+            {
+                while (k >= coins[i])
+                {
+                    k -= coins[i];
+                    answer++;
+                }
+            }
+            
+            Console.WriteLine(answer);
+        }
+    }
+}
