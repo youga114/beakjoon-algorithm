30eb8af3a7d6fc388308af5c2e7cd4aa9bc6ff7d
yubie <youga114@gmail.com>
2022-05-26 18:33:37 +0900

    [10757] Add answer

---
+++ problem/10757/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10757
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ab = Console.ReadLine().Split(" ").ToList();
+            string a = ab[0], b = ab[1];
+
+            int maxCnt = 0;
+            if (a.Length >= b.Length)
+            {
+                maxCnt = a.Length;
+            }
+            else
+            {
+                maxCnt = b.Length;
+            }
+
+            int nextNum = 0;
+            string answer = "";
+            for (int i = 0; i < maxCnt; ++i)
+            {
+                char aChar = '0', bChar = '0';
+                if (a.Length - i - 1 >= 0)
+                {
+                    aChar = a[a.Length - i - 1];
+                }
+
+                if (b.Length - i - 1 >= 0)
+                {
+                    bChar = b[b.Length - i - 1];
+                }
+
+                int sum = Convert.ToInt32(aChar.ToString()) + Convert.ToInt32(bChar.ToString()) + nextNum;
+                if (sum >= 10)
+                {
+                    nextNum = 1;
+                    sum -= 10;
+                }
+                else
+                {
+                    nextNum = 0;
+                }
+
+                answer = sum.ToString() + answer;
+            }
+
+            if (nextNum == 1)
+            {
+                answer = "1" + answer;
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
