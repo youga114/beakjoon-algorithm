0c100499b3eed7ca2c959cfa99fbb8225bdd6b1a
yubie <youga114@gmail.com>
2022-05-31 16:52:00 +0900

    [2164] Add answer

---
+++ problem/2164/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2164
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            var allCard = new List<bool>(new bool[n + 1]);
+
+            int idx = 2, prevIdx = 1, cnt = 0;
+            allCard[1] = true;
+            if (idx > n)
+            {
+                idx = 1;
+            }
+
+            while (idx != prevIdx)
+            {
+                if (allCard[idx] == false)
+                {
+                    cnt++;
+                    prevIdx = idx;
+                }
+
+                if (cnt == 2)
+                {
+                    cnt = 0;
+                    allCard[idx] = true;
+                }
+                
+                idx++;
+                if (idx > n)
+                {
+                    idx = 1;
+                }
+            }
+            
+            Console.WriteLine(idx);
+        }
+    }
+}
