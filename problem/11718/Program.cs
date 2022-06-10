da03b286bbf8b4aafe47a2f1293f148b4a82ee54
yubie <youga114@gmail.com>
2022-06-10 18:43:33 +0900

    [11718] Add answer

---
+++ problem/11718/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11718
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+
+            while (str != null)
+            {
+                Console.WriteLine(str);
+                str = Console.ReadLine();
+            }
+        }
+    }
+}
