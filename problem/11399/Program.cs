f1e21b21a74280ef2df774e1d2bbf02160eb0b1e
yubie <youga114@gmail.com>
2022-06-10 12:42:23 +0900

    [11399] Add answer

---
+++ problem/11399/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _11399
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            var waiting = Console.ReadLine().Split(" ").Select((x) => Convert.ToInt32(x)).ToList();
+            int answer = 0;
+            
+            waiting.Sort();
+            for (int i = 0; i < n; ++i)
+            {
+                answer += waiting[i] * (n - i);
+            }
+            
+            Console.WriteLine(answer);
+        }
+    }
+}
