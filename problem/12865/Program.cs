a4038131d0be11a2933a1fa1a5b9a73f992d7cfa
yubie <youga114@gmail.com>
2022-06-22 18:01:00 +0900

    [12865] Add answer

---
+++ problem/12865/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _12865
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nk = Console.ReadLine().Split(" ").Select((x) => Convert.ToInt32(x)).ToList();
+            int n = nk[0], k = nk[1];
+
+            var bag = new List<int>(new int[k + 1]);
+            for (int i = 1; i < k + 1; ++i)
+            {
+                bag[i] = -1;
+            }
+            
+            int max = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                var weightAndValue = Console.ReadLine().Split(" ").Select((x) => Convert.ToInt32(x)).ToList();
+                int weight = weightAndValue[0], value = weightAndValue[1];
+
+                for (int j = k; j >= weight; --j)
+                {
+                    if (bag[j - weight] + value > bag[j] )
+                    {
+                        bag[j] = bag[j - weight] + value;
+                        if (bag[j] > max)
+                        {
+                            max = bag[j];
+                        }
+                    }
+                }
+            }
+            
+            Console.WriteLine(max);
+        }
+    }
+}
