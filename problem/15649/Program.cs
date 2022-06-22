716e17328da52379d830b5d3dc5cfa47f379def8
yubie <youga114@gmail.com>
2022-06-22 15:23:45 +0900

    [15649] Add answer

---
+++ problem/15649/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _15649
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nm = Console.ReadLine().Split(" ").Select((x) => Convert.ToInt32(x)).ToList();
+            int n = nm[0], m = nm[1];
+
+            StringBuilder sb = new StringBuilder();
+            var answer = new List<int>(new int[m]);
+            var visited = new List<bool>(new bool[n + 1]);
+            dfs(n, m, 0, visited, answer, sb);
+            
+            Console.WriteLine(sb.ToString());
+        }
+
+        static void dfs(int n, int m, int curIdx, List<bool> visited, List<int> answer, StringBuilder sb)
+        {
+            if (curIdx >= m)
+            {
+                foreach (var i in answer)
+                {
+                    sb.Append(i + " ");
+                }
+
+                sb.Append("\n");
+
+                return;
+            }
+            
+            for (int i = 1; i <= n; ++i)
+            {
+                if (visited[i] == false)
+                {
+                    visited[i] = true;
+                    answer[curIdx] = i;
+                    dfs(n, m, curIdx + 1, visited, answer, sb);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+}
