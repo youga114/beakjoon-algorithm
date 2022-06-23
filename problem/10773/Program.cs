d028b3e5097cb7ac55797210cfcef942126a9af7
yubie <youga114@gmail.com>
2022-06-23 11:55:20 +0900

    [10773] Add answer

---
+++ problem/10773/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10773
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; ++i)
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                if (num == 0)
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(num);
+                }
+            }
+
+            int answer = 0;
+            foreach (var i in stack)
+            {
+                answer += i;
+            }
+            
+            Console.WriteLine(answer);
+        }
+    }
+}
