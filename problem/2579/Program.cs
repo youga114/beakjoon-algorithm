7f495c26af3f993263f4e511016e2c3efc8bc35b
yubie <youga114@gmail.com>
2022-06-30 11:55:21 +0900

    [2579] Add answer

---
+++ problem/2579/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2579
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            var stairs = new List<int>(new int[1]);
+            
+            for (int i = 0; i < n; ++i)
+            {
+                stairs.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            var dp = new List<int>(new int[n + 1]);
+            dp[1] = stairs[1];
+
+            if (n > 1)
+            {
+                dp[2] = stairs[1] + stairs[2];
+            }
+
+            for (int i = 3; i < n + 1; ++i)
+            {
+                int maxValue = (dp[i - 2] > stairs[i - 1] + dp[i - 3]) ? dp[i - 2] : stairs[i - 1] + dp[i - 3];
+                dp[i] = stairs[i] + maxValue;
+            }
+            
+            Console.WriteLine(dp[n]);
+        }
+    }
+}
