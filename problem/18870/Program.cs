7642a85c328cc37f6460040db75363626eddc03c
yubie <youga114@gmail.com>
2022-07-04 14:35:00 +0900

    [18870] Add answer

---
+++ problem/18870/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _18870
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            var nums = Console.ReadLine().Split(" ").Select((x) => Convert.ToInt32(x)).ToList();
+
+            var sortedNums = new List<int>(nums);
+            sortedNums.Sort();
+            sortedNums = sortedNums.Distinct().ToList();
+
+            StringBuilder sb = new StringBuilder();
+            
+            for (int i = 0; i < n; ++i)
+            {
+                sb.Append(sortedNums.BinarySearch(nums[i]) + " ");
+            }
+            
+            Console.WriteLine(sb.ToString());
+        }   
+    }
+}
