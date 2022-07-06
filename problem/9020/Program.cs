364c1d5e91c10e5fefa6c1cc07c34af1f6a22df3
yubie <youga114@gmail.com>
2022-07-06 12:26:19 +0900

    [9020] Add answer

---
+++ problem/9020/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _9020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            var isPrime = new List<bool>(new bool[10001]);
+            
+            for (int i = 2; i < 10001; ++i)
+            {
+                isPrime[i] = true;
+            }
+            for (int i = 2; i < 10001; ++i)
+            {
+                if (isPrime[i] == true)
+                {
+                    for (int j = i * i; j < 10001; j += i)
+                    {
+                        isPrime[j] = false;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; ++i)
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                int leftNum = num / 2, rightNum = num / 2;
+
+                while (isPrime[leftNum] == false || isPrime[rightNum] == false)
+                {
+                    leftNum--;
+                    rightNum++;
+                }
+                
+                Console.WriteLine(leftNum + " " + rightNum);
+            }
+        }   
+    }
+}
