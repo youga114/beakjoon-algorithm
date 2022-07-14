d58175fdf57d2d6d4f755651e7a6f749f19dab82
yubie <youga114@gmail.com>
2022-07-14 18:00:11 +0900

    [15651] Add answer

---
+++ problem/15651/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _15651
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nm = Console.ReadLine().Split(" ").Select((x) => Convert.ToInt32(x)).ToList();
+            int n = nm[0], m = nm[1];
+
+            var answer = new List<int>(new int[m]);
+            var sb = new StringBuilder();
+            
+            dfs(answer, 0, n, m, sb);
+            
+            Console.WriteLine(sb.ToString());
+        }
+
+        static void dfs(List<int> answer, int idx, int n, int m, StringBuilder sb)
+        {
+            if (idx >= m)
+            {
+                for (int i = 0; i < m; ++i)
+                {
+                    sb.Append(answer[i] + " ");
+                }
+
+                sb.Append("\n");
+                return;
+            }
+
+            for (int i = 1; i <= n; ++i)
+            {
+                answer[idx] = i;
+                dfs(answer, idx + 1, n, m, sb);
+            }
+        }
+    }
+}
