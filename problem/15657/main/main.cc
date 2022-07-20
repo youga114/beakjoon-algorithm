97cb275b16430148e672ffd75667b55d1df092da
yubie <youga114@gmail.com>
2022-07-20 12:37:16 +0900

    [15657] Add answer

---
+++ problem/15657/main/main.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void dfs(int n, int m, vector<int> v, vector<int> result, int curIdx, int prevIdx)
+{
+    if (curIdx >= m)
+    {
+        for (int i = 0; i < m; ++i)
+        {
+            cout << result[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = prevIdx; i < n; ++i)
+    {
+        result[curIdx] = v[i];
+        dfs(n, m, v, result, curIdx + 1, i);
+    }
+}
+
+int main(int argc, char** argv) {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m;
+
+    cin >> n >> m;
+
+    vector<int> v(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    vector<int> result(m);
+
+    dfs(n, m, v, result, 0, 0);
+
+    return 0;
+}
