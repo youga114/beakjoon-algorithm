f89f6d56bfe810c846a8887746c881e5af0bb9f4
youga114 <youga114@gmail.com>
2023-05-17 18:25:21 +0900

    [c#] Add files

---
+++ problem/1676/problem/problem/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _1676
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int zeroCount = 0;
+
+            int twoCount = 0, fiveCount = 0;
+            for (int i = 1; i < n + 1; ++i)
+            {
+                int num = i;
+                while (num % 2 == 0)
+                {
+                    num /= 2;
+                    twoCount++;
+                }
+
+                while (num % 5 == 0)
+                {
+                    num /= 5;
+                    fiveCount++;
+                }
+            }
+            
+            Console.WriteLine(Math.Min(twoCount, fiveCount));
+        }
+    }
+}
---
+++ problem/2004/problem/problem/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _2004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nm = Console.ReadLine().Split(" ").Select((str) => Convert.ToInt32(str)).ToArray();
+            int n = nm[0], m = nm[1];
+
+            int fiveCount = 0;
+            int squareFive = 5;
+            while (squareFive <= n)
+            {
+                fiveCount += n / squareFive;
+                squareFive *= 5;
+            }
+
+            squareFive = 5;
+            while (squareFive <= m)
+            {
+                fiveCount -= m / squareFive;
+                squareFive *= 5;
+            }
+
+            squareFive = 5;
+            while (squareFive <= n - m)
+            {
+                fiveCount -= (n - m) / squareFive;
+                squareFive *= 5;
+            }
+
+            int twoCount = 0;
+            int squareTwo = 2;
+            while (squareTwo <= n)
+            {
+                twoCount += n / squareTwo;
+                squareTwo *= 2;
+            }
+
+            squareTwo = 2;
+            while (squareTwo <= m)
+            {
+                twoCount -= m / squareTwo;
+                squareTwo *= 2;
+            }
+
+            squareTwo = 2;
+            while (squareTwo <= n - m)
+            {
+                twoCount -= (n - m) / squareTwo;
+                squareTwo *= 2;
+            }
+            
+            Console.WriteLine(Math.Min(fiveCount, twoCount));
+        }
+    }
+}
