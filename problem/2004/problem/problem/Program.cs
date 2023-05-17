6177c82e4b67ae0bf6c693c63d88f7e48348d285
youga114 <youga114@gmail.com>
2023-05-17 18:33:58 +0900

    [2004] Add answer

--- problem/2004/problem/problem/Program.cs
+++ problem/2004/problem/problem/Program.cs
@@ -2,13 +2,15 @@
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            int[] nm = Console.ReadLine().Split(" ").Select((str) => Convert.ToInt32(str)).ToArray();
-            int n = nm[0], m = nm[1];
-
-            int fiveCount = 0;
-            int squareFive = 5;
+            long[] nm = Console.ReadLine().Split(" ").Select((str) => Convert.ToInt64(str)).ToArray();
+            long n = nm[0], m = nm[1];
+            long fiveCount = 0;
+            long twoCount = 0;
+            long squareFive = 5;
+            long squareTwo = 2;
+            
             while (squareFive <= n)
             {
                 fiveCount += n / squareFive;
@@ -29,8 +31,6 @@
                 squareFive *= 5;
             }
 
-            int twoCount = 0;
-            int squareTwo = 2;
             while (squareTwo <= n)
             {
                 twoCount += n / squareTwo;
@@ -52,6 +52,8 @@
             }
             
             Console.WriteLine(Math.Min(fiveCount, twoCount));
+
+            return 0;
         }
     }
 }
